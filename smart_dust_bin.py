71c1ba1d13916bc464331f7b8eeca13e784ccfc6
Suraj S Jain <surajsjain@hotmail.com>
2018-02-25 17:54:13 +0530

    First program with getDistance function

---
+++ smart_dust_bin.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+def getDistance():
+    trig=4
+    echo=18
+
+    GPIO.setup(trig, GPIO.OUT)
+    GPIO.setup(echo, GPIO.IN)
+
+    GPIO.output(trig, True)
+    time.sleep(0.0001)
+    GPIO.output(trig, False)
+
+    while GPIO.input(echo) == False:
+        start=time.time()
+
+    while GPIO.input(echo) == True:
+        end=time.time()
+
+    sig_time= end-start
+
+    distance= sig_time/0.000058
+
+    return(distance)
+
+print str(getDistance())
